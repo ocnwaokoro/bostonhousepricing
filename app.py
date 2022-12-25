9e1b72a8ba667a1ddb5b226267ac91b74b1e4be1
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2022-12-25 16:01:59 -0500

    webapp

---
+++ app.py
@@ -0,0 +1,38 @@
+import pickle
+import json
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+import sklearn
+
+app = Flask(__name__)
+# Load the model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
+
+# The first route
+@app.route('/')
+# home page
+def home():
+    return render_template('home.html')
+
+# create predict api
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    # whenever the predict api is pressed, data is given in json format & stored in data
+    data = request.json['data']
+    print(data)
+    # in json it will be in key value pairs so you need to get the values
+    # single datapoint record that you get
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    # output is 2-d array
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
